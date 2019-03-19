c97f5a41e707039db5715f2c2819bc7f9dd4e7b2
MadelaineS <s.madelaine@gmail.com>
2019-03-19 14:10:51 +0200

    added gulp with gulp-sass and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,54 @@
 'use strict';
+/*jslint node: true */
 
-var gulp = require('gulp');
+/*jshint esversion: 6 */
 
-var sass = require('gulp-sass');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create();
 
-// gulp.task("hello", async function(){
-// 	console.log("Hello!");
-// });
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/styles.scss')
+//compile sass into css & reload browser
+function style() {
+    return gulp
+        .src('app/scss/**/*.scss') //pinpoints to scss file
+        .pipe(sourcemaps.init()) //initializes sourcemaps before compilation starts
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
-});
+        .on('error', sass.logError) //pass the scss file through the compiler
+        .pipe(postcss([autoprefixer(), cssnano()])) //used postcss with autoprefixer and compress the compiled file using cssnano
+        .pipe(sourcemaps.write()) //add sourcemaps
+        .pipe(gulp.dest('app/css')) //where to save the compiled css
+        .pipe(browserSync.stream()); //stream changes to the browser
+}
 
-gulp.task('watch', function() {
-    gulp.watch('app/scss/styles.scss', gulp.series('sass'));
-})
+
+//reloads the browser
+function reload() {
+    browserSync.reload();
+}
+
+function watch() {
+    //initializes static localhost server
+    browserSync.init({
+        server: {
+            baseDir: './app'
+        }
+    });
+
+    //watches sass file and html/js
+    gulp.watch('app/scss/**/*.scss', style);
+    //reloads browser when HTML/JS files change
+    gulp.watch('app/*.html', reload);
+    gulp.watch('app/js/*.js', reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
+
+var build = gulp.parallel(style, watch); //runs tasks in parallel
+gulp.task('default', build);
